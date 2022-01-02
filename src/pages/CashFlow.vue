0a3fd8d8db74c6e1ea619fab3c3ff55387163c77
Deven Phillips <deven.phillips@redhat.com>
2022-01-02 21:46:09 +0000

    Started working on cash flow visualization

--- src/pages/CashFlow.vue
+++ src/pages/CashFlow.vue
@@ -1,12 +1,51 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <div class="flex q-md-pa padding">
+      <q-input
+        v-model.number="startingBalance"
+        label="Starting Balance"
+        :prefix="currencySymbol()"
+      />
+      <q-input v-model="startDate" type="date" label="Start Date" />
+      <q-input v-model="endDate" type="date" label="End Date" />
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue';
+import { date } from 'quasar';
+
+const { addToDate } = date;
+
 export default defineComponent({
   // name: 'PageName'
-})
+  setup() {
+    const startingBalance = ref(0.0);
+
+    const startDate = ref(addToDate(new Date, { months: -1 }));
+    const endDate = ref(addToDate(new Date, { months: 1 }));
+
+    const currency = ref('USD');
+
+    const currencySymbol = () => {
+      switch (currency.value) {
+        case 'USD':
+          return '$';
+        case 'GBP':
+          return '£';
+        default:
+          return '€';
+      }
+    };
+
+    return {
+      startingBalance,
+      startDate,
+      endDate,
+      currency,
+      currencySymbol,
+    };
+  },
+});
 </script>
