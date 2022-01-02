d0875fca1ce234cebfe0a9983e86039102aee133
Deven Phillips <deven.phillips@redhat.com>
2022-01-02 21:05:48 +0000

    Added new routes for income and cashflow

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,17 +1,22 @@
 import { RouteRecordRaw } from 'vue-router';
+import Index from 'src/pages/Index.vue';
+import EditBill from 'src/pages/EditBill.vue';
+import IncomeEdit from 'src/pages/IncomeEdit.vue';
+import IncomeList from 'src/pages/IncomeList.vue';
+import CashFlow from 'src/pages/CashFlow.vue';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') },
-      { path: '/bill/add', component: () => import('pages/EditBill.vue') },
-      { path: '/bill/edit/:id', component: () => import('pages/EditBill.vue'), props: true },
-      { path: '/income', component: () => import('pages/IncomeList.vue') },
-      { path: '/income/add', component: () => import('pages/EditIncome.vue') },
-      { path: '/income/edit/:id', component: () => import('pages/EditIncome.vue'), props: true },
-      { path: '/cashflow', component: () => import('pages/CashFlow.vue') },
+      { path: '', component: Index },
+      { path: '/bill/add', component: EditBill },
+      { path: '/bill/edit/:id', component: EditBill, props: true },
+      { path: '/income/list', component: IncomeList },
+      { path: '/income/add', component: IncomeEdit },
+      { path: '/income/edit/:incomeId', component: IncomeEdit, props: true },
+      { path: '/cashflow', component: CashFlow },
     ],
   },
 
