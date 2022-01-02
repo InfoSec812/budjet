7ffc9d9ddd5db5e91937282202c601f07298c6fd
Deven Phillips <deven.phillips@redhat.com>
2022-01-02 19:57:13 +0000

    Added routes for CashFlow and Income

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,6 +8,10 @@ const routes: RouteRecordRaw[] = [
       { path: '', component: () => import('pages/Index.vue') },
       { path: '/bill/add', component: () => import('pages/EditBill.vue') },
       { path: '/bill/edit/:id', component: () => import('pages/EditBill.vue'), props: true },
+      { path: '/income', component: () => import('pages/IncomeList.vue') },
+      { path: '/income/add', component: () => import('pages/EditIncome.vue') },
+      { path: '/income/edit/:id', component: () => import('pages/EditIncome.vue'), props: true },
+      { path: '/cashflow', component: () => import('pages/CashFlow.vue') },
     ],
   },
 
