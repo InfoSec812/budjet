bb893d73de0fc78e60c4734ee13adefcfeb58ced
Deven Phillips <deven.phillips@redhat.com>
2022-03-07 07:31:30 -0500

    Add pot of gold

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,16 @@
 <template>
   <q-page class="row items-center justify-evenly">
+    <img src="pog.png" class="floating-image" alt="Pot O' Gold" />
   </q-page>
 </template>
 
 <script setup lang="ts">
 </script>
+<style scoped lang="sass">
+.floating-image
+  z-index: 5
+  position: absolute
+  bottom: 1rem
+  right: 1rem
+  width: 5rem
+</style>
